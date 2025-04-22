241d8f5f91b2e2195bf3d8b473cdf72fed980c0c
Abhijan Mallick <mallickabhijan23@gmail.com>
2025-04-23 00:14:19 +0530

    Updated Commit

--- backend/employee-management/src/main/java/com/example/employeemanagement/controller/EmployeeController.java
+++ backend/employee-management/src/main/java/com/example/employeemanagement/controller/EmployeeController.java
@@ -1,9 +1,16 @@
 package com.example.employeemanagement.controller;
 
+import com.example.employeemanagement.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/employees")
+@CrossOrigin(origins = "*")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
 }
